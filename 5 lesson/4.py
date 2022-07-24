41df472a248eaf2d6c5544493b3d647ccd76de30
chlvek <guno.30pm@gmail.com>
2022-07-24 16:21:50 +0300

    task complete

---
+++ 5 lesson/4.py
@@ -0,0 +1,42 @@
+#4 -Реализуйте RLE алгоритм:
+# реализуйте модуль сжатия и восстановления данных.Входные и выходные данные хранятся в отдельных текстовых файлах
+
+def compress(data):
+    '''RLE compress.'''
+
+    compressed = ''
+    current_count = 0
+    current_symbol = data[0]
+    for s in data:
+        if s != current_symbol:
+            if current_count > 1:
+                compressed += f'{current_count}{current_symbol}'
+            else:
+                compressed += current_symbol
+            current_symbol = s
+            current_count = 1
+        else:
+            current_count += 1
+
+    if current_count > 1:
+        compressed += f'{current_count}{current_symbol}'
+    else:
+        compressed += current_symbol
+    return compressed
+
+
+if __name__ == "__main__":
+    original_file_path = 'original'
+    compressed_file_path = 'compressed'
+
+    with open(original_file_path, 'r') as file:
+        data = file.read()
+
+    print(f'Initial data:\n{data}')
+
+    compressed = compress(data)
+
+    print(f'Compressed data:\n{compressed}')
+
+    with open(compressed_file_path, 'w') as file:
+        file.write(compressed)
